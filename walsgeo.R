6a5266316102916ff9d8c37fafed374309518ee2
Henri Kauhanen <nenahuak@gmail.com>
2020-01-09 17:59:31 +0100

    updated walsgeo

--- walsgeo.R
+++ walsgeo.R
@@ -15,22 +15,3 @@ walsgeo <- function(languages = unique(ritwals::WALS_languages$language_ID)) {
   do.call(rbind, lapply(X=languages, FUN=myfun))
 }
 
-# Break down language-by-language - github has 100MB file limit :((((((
-#
-breakdown <- function(df,
-		      folder = "languages") {
-	lges <- unique(df$language_ID)
-	for (lge in lges) {
-		thisdf <- df[df$language_ID==lge, ]
-		write.csv(thisdf, file=paste0(folder, "/", lge, ".csv"), row.names=FALSE)
-	}
-}
-
-# Break down into three parts
-#
-breakdown2 <- function(df) {
-	N <- round(nrow(df)/3)
-	write.csv(df[1:N, ], file="wals-distances-1.csv", row.names=FALSE)
-	write.csv(df[(N+1):(2*N), ], file="wals-distances-2.csv", row.names=FALSE)
-	write.csv(df[(2*N+1):nrow(df), ], file="wals-distances-3.csv", row.names=FALSE)
-}
